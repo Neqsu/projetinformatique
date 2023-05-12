d36c1b9595bb83df3b955f6f7a7ea3059879867f
NaTwiX27 <133356569+NaTwiX27@users.noreply.github.com>
2023-05-12 15:25:16 +0200

    Update stuff.c

--- stuff.c
+++ stuff.c
@@ -1,3 +1,23 @@
 #include <stdio.h>
 #include "header.h"
 
+void graphic_interface(){
+printf("POSITIONS DE DEPART        \n");
+printf("Guerrier [▣]  [▤] Ranger  \n");
+printf("Magicien [▦]  [▩] Voleur  \n");
+printf("\n");
+printf("AVENTURIERS                \n");
+printf("Guerrier [❃]  [❖] Ranger  \n");
+printf("Magicien [✯]  [✱] Voleur  \n");
+printf("\n");
+printf("TRESORS                    \n");
+printf("Bâton    [|]  [⚔] Épée     \n");
+printf("Grimoire [≡]  [↗]  Dague   \n");
+printf("Coffre trésor [☒]          \n");
+printf("\n");
+printf("LABYRINTHE                 \n");
+printf("Inconnu  [◫]  [▢]  Vide    \n");
+printf("Basilic  [〠] [🧟] Zombie  \n");
+printf("Troll    [㋡] [👾] Harpie  \n");
+printf("Portail  [◆] [☫]  Totem   \n");
+}
