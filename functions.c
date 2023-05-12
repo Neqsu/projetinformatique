92061bedb59ff89dc58e2b8f41390ee4811290c9
NaTwiX27 <133356569+NaTwiX27@users.noreply.github.com>
2023-05-12 12:47:40 +0200

    Update functions.c

--- functions.c
+++ functions.c
@@ -8,10 +8,10 @@ void game_turn(int choice){
 	a.stone_axe = 3;
 	a.longbow = 4;
 	while(choice != "1" && choice != "2" && choice != "3" && choice != "4"){
-	  printf("Veuillez choisir une arme : \n");
-	  printf("1 - Torche");
-	  printf("2 - Bouclier\n");
-	  printf("3 - Hache\n");
-	  printf("4 - Arc\n");
-	  scanf("%d", &choice);
-  }
+		printf("Veuillez choisir une arme : \n");
+	  	printf("1 - Torche");
+	  	printf("2 - Bouclier\n");
+	  	printf("3 - Hache\n");
+		printf("4 - Arc\n");
+  	}
+        scanf("%d", &choice);
