a23db2060632a35e3b92df81c74f36d2655443dd
NaTwiX27 <133356569+NaTwiX27@users.noreply.github.com>
2023-05-12 15:27:12 +0200

    Update stuff.c

--- stuff.c
+++ stuff.c
@@ -2,22 +2,22 @@
 #include "header.h"
 
 void graphic_interface(){
-printf("POSITIONS DE DEPART        \n");
-printf("Guerrier [▣]  [▤] Ranger  \n");
-printf("Magicien [▦]  [▩] Voleur  \n");
-printf("\n");
-printf("AVENTURIERS                \n");
-printf("Guerrier [❃]  [❖] Ranger  \n");
-printf("Magicien [✯]  [✱] Voleur  \n");
-printf("\n");
-printf("TRESORS                    \n");
-printf("Bâton    [|]  [⚔] Épée     \n");
-printf("Grimoire [≡]  [↗]  Dague   \n");
-printf("Coffre trésor [☒]          \n");
-printf("\n");
-printf("LABYRINTHE                 \n");
-printf("Inconnu  [◫]  [▢]  Vide    \n");
-printf("Basilic  [〠] [🧟] Zombie  \n");
-printf("Troll    [㋡] [👾] Harpie  \n");
-printf("Portail  [◆] [☫]  Totem   \n");
+  printf("POSITIONS DE DEPART        \n");
+  printf("Guerrier [▣]  [▤] Ranger  \n");
+  printf("Magicien [▦]  [▩] Voleur  \n");
+  printf("\n");
+  printf("AVENTURIERS                \n");
+  printf("Guerrier [❃]  [❖] Ranger  \n");
+  printf("Magicien [✯]  [✱] Voleur  \n");
+  printf("\n");
+  printf("TRESORS                    \n");
+  printf("Bâton    [|]  [⚔] Épée     \n");
+  printf("Grimoire [≡]  [↗]  Dague   \n");
+  printf("Coffre trésor [☒]          \n");
+  printf("\n");
+  printf("LABYRINTHE                 \n");
+  printf("Inconnu  [◫]  [▢]  Vide    \n");
+  printf("Basilic  [〠] [🧟] Zombie  \n");
+  printf("Troll    [㋡] [👾] Harpie  \n");
+  printf("Portail  [◆] [☫]  Totem   \n");
 }
