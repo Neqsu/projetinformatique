92ede5e4d7b74a096deb63f9ad93148c2b544def
noraagrgl <131977915+noraagrgl@users.noreply.github.com>
2023-05-27 22:32:40 +0200

    Update header.h

--- header.h
+++ header.h
@@ -1,56 +1,83 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
 #ifndef HEADER_H
 #define HEADER_H
-
-typedef struct{
-	int warrior;
-	int ranger;
-	int wizard;
-	int thief;
-}Adventurers;
-
-typedef struct{
-	int fire_sword;
-	int pet_control_stick;
-	int forbidden_grimoire;
-	int sleep_dagger;
-}Antique_Weapons;
-
-typedef struct{
-	int reflective_shield;
-	int torch;
-	int longbow;
-	int stone_axe;
-}Normal_Weapons;
-
-typedef struct{
-	int basilisks;
-	int zombies;
-	int trolls;
-	int harpies;
-}Monsters_Categories;
-
-typedef struct{
-	Antique_Weapons boxes;
-	int treasure_chests;
-	Monsters_Categories monsters;
-	int teleport_magic_portal;
-	int transmutation_totems;
-}Maze_boxes;
-
-#endif
+#define PLAYERS 4
+#define NB_ROLE 4
+#define CARD_BACK "🂠"
+#define SIZE_PLATEAU 5
+#define EXTRA_SIZE 1
+#define TOTAL_SIZE (SIZE_PLATEAU + EXTRA_SIZE)
 
 typedef enum {
-    FIRE_SWORD,
-    STICK,
-    FORBIDDEN_GRIMOIRE,
-    SLEEP_DAGGER,
-    TREASURE_CHEST,
-    BASILISK,
-    ZOMBIE,
-    TROLL,
-    HARPY,
-    TELEPORT_MAGIC_PORTAL,
-    TRANSMUTATION_TOTEM,
-    EMPTY,
-    HIDDEN,
-} T_cards;
+  FIRE_SWORD,
+  STICK,
+  FORBIDDEN_GRIMOIRE,
+  SLEEP_DAGGER,
+  TREASURE_CHEST,
+  BASILISK,
+  ZOMBIE,
+  TROLL,
+  HARPY,
+  HIDDEN,
+  PLAYERPOS,
+  TELEPORT_MAGIC_PORTAL,
+  TRANSMUTATION_TOTEM,
+  EMPTY,
+} T_Cards;
+
+typedef enum {
+  WARRIOR,
+  RANGER,
+  MAGE,
+  THIEF,
+} T_Role;
+
+typedef enum {
+  TORCH,
+  SHIELD,
+  AXE,
+  BOW,
+  NO_WEAPON,
+} Weapon;
+
+typedef struct {
+  char name[50];
+  int num;
+  T_Role jrole;
+  T_Cards role;
+  Weapon weapon;
+  int alive;
+  int treasureFound;
+  int nb_cases;
+  int chest;
+  int special_obj;
+  int x;
+  int y;
+} Player;
+
+void vide_buffer();
+
+void rules();
+
+int nb_players();
+
+Weapon chooseWeapon(Player player);
+
+void initialize_players(Player *players, int NB_PLAYER);
+
+void copy_gameboard(T_Cards source[SIZE_PLATEAU][SIZE_PLATEAU], T_Cards destination[SIZE_PLATEAU][SIZE_PLATEAU]);
+
+void player_turn(Player *player, T_Cards gameboard[SIZE_PLATEAU][SIZE_PLATEAU], copied_gameboard[SIZE_PLATEAU][SIZE_PLATEAU], int NB_PLAYER);
+
+void create_table(T_Cards gameBoard[SIZE_PLATEAU][SIZE_PLATEAU]);
+
+void print_table(T_Cards gameBoard[SIZE_PLATEAU][SIZE_PLATEAU]);
+
+void return_cards(T_Cards gameBoard[SIZE_PLATEAU][SIZE_PLATEAU]);
+
+void print_Plateau(T_Cards tab_plateau[SIZE_PLATEAU][SIZE_PLATEAU]);
+
+#endif
