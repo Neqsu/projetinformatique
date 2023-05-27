6ad7f976a65cc97171842bf32c12911d04df96a4
noraagrgl <131977915+noraagrgl@users.noreply.github.com>
2023-05-27 23:57:10 +0200

    Update functions.c

--- functions.c
+++ functions.c
@@ -10,14 +10,16 @@ void rules(){
     printf("=-=-=-=-=-=-=-=-=-=-=-=-=-=\n");
     printf("Welcome to CY_Memory-RPG !\n");
     printf("=-=-=-=-=-=-=-=-=-=-=-=-=-=\n");
-    printf("\nThe aim of the game for adventurers is to navigate the maze in search of a treasure chest and an ancient weapon, without getting killed by the monsters.\nThere are 4 adventurers who seek 4 weapons : the warrior seeks the sword, the ranger the staff, the magician the grimoire and the thief the dagger.\nYou can kill the monsters : the basilisk with a mirror, the zombie with a torch, the troll with an ax and the harpy with a bow\n");
+    printf("\nThe aim of the game for adventurers is to navigate the maze in search of a treasure chest and an ancient weapon, without getting killed by the monsters.\nThere are 4 adventurers who seek 4 weapons : the warrior seeks the sword, the ranger seeks the staff, the magician seeks the grimoire and the thief seeks the dagger.\nYou can kill the monsters : the basilisk with a mirror, the zombie with a torch, the troll with an ax and the harpy with a bow.\n");
 }
 
 // Choice of the number of players
 int nb_players() {
   int NB_PLAYER;
+  
   printf("Select the number of players (2-4) : \n");
   scanf("%d", &NB_PLAYER);
+  
   if (NB_PLAYER < 2 || NB_PLAYER > 4) {
     while (NB_PLAYER < 2 || NB_PLAYER > 4) {
       printf("ERROR : Please choose between 2 and 4\n");
@@ -80,8 +82,8 @@ void initialize_players(Player *players, int NB_PLAYER) {
                 break;
         }
         scanf("%s", players[i].name);
-        players[i].x = i == WARRIOR ? 0 : (i == RANGER ? 3 : (i == MAGE ? 4 : 1));
-        players[i].y = i == WARRIOR ? 1 : (i == RANGER ? 0 : (i == MAGE ? 3 : 4));
+        players[i].x = i == RANGER ? 3 : (i == WARRIOR ? 0 : (i == MAGE ? 3 : 4));
+        players[i].y = i == RANGER ? 0 : (i == WARRIOR ? 3 : (i == MAGE ? 4 : 1));
         players[i].weapon = NO_WEAPON;
         players[i].special_obj = 0;
         players[i].chest = 0;
@@ -101,10 +103,11 @@ void copy_gameboard(T_Cards source[SIZE_PLATEAU][SIZE_PLATEAU], T_Cards destinat
 // Player turn
 void player_turn(Player *player, T_Cards gameboard[SIZE_PLATEAU][SIZE_PLATEAU], copied_gameboard[SIZE_PLATEAU][SIZE_PLATEAU], int NB_PLAYER) {
     char direction;
+    player->alive = 0;
     return_cards(copied_gameboard);
     const char* role_names[] = {
-        "WARRIOR",
         "RANGER",
+        "WARRIOR",
         "MAGE",
         "THIEF"
     };
@@ -120,24 +123,57 @@ const char* weapon_names[] = {
 
 printf("Weapon: %s\n", weapon_names[player->weapon]);
 
-if (player->jrole == WARRIOR) {
-    printf("You are the warrior and you start on the green square, you are automatically teleported to the nearest square\n");
-    player->x = 0;
+  if (player->jrole == RANGER) {
+    printf("You are the ranger and you start on the green square, you are automatically teleported to the nearest square\n");
+    player->x = 0 ;
     player->y = 1;
-} else if (player->jrole == RANGER) {
-    printf("You are the ranger and you start on the purple square, you are automatically teleported to the nearest square\n");
+    copied_gameboard[player->y][player->x] = gameboard[player->y][player->x];
+    print_Plateau(copied_gameboard);
+  } 
+  
+  else if (player->jrole == WARRIOR) {
+    printf("You are the warrior and you start on the purple square, you are automatically teleported to the nearest square\n");
+    copied_gameboard[player->y][player->x] = gameboard[player->y][player->x];
+    print_Plateau(copied_gameboard);
     player->x = 3;
     player->y = 0;
-} else if (player->jrole == MAGE) {
+  } 
+  
+  else if (player->jrole == MAGE) {
     printf("You are the mage and you start on the orange square, you are automatically teleported to the nearest square\n");
     player->x = 4;
     player->y = 3;
-} else {
+    copied_gameboard[player->y][player->x] = gameboard[player->y][player->x];
+    print_Plateau(copied_gameboard);
+  } 
+  
+  else {
     printf("You are the thief and you start on the brown square, you are automatically teleported to the nearest square\n");
     player->x = 1;
     player->y = 4;
-}
+  copied_gameboard[player->y][player->x] = gameboard[player->y][player->x];
+    print_Plateau(copied_gameboard);
+  }
+  T_Cards start_card = gameboard[player->y][player->x];
+    const char* card_names[] = {
+        "EMPTY",
+        "MONSTER",
+        "POTION",
+        "TREASURE",
+        "ZOMBIE",
+        "TROLL",
+        "BASILISK",
+        "HARPY",
+        "FIRE_SWORD",
+        "STICK",
+        "FORBIDDEN_GRIMOIRE",
+        "SLEEP_DAGGER",
+        "TREASURE_CHEST",
+        "TELEPORT_MAGIC_PORTAL",
+        "TRANSMUTATION_TOTEM"
+    };
 
+    printf("You are on a %s square.\n", card_names[start_card]);
 printf("\nChoose a direction:\n\n          z)UP\n  q) LEFT      d) RIGHT\n         s)DOWN\n\n");
 
 do {
@@ -148,7 +184,8 @@ do {
         case 'z':
             if (player->y > 0) {
                 player->y--;
-            } else {
+            } 
+            else {
                 printf("Stay on the plateau!\n");
                 continue;
             }
@@ -157,7 +194,8 @@ do {
         case 'q':
             if (player->x > 0) {
                 player->x--;
-            } else {
+            } 
+            else {
                 printf("Stay on the plateau!\n");
                 continue;
             }
@@ -166,7 +204,8 @@ do {
         case 's':
             if (player->y < (SIZE_PLATEAU - 1)) {
                 player->y++;
-            } else {
+            } 
+            else {
                 printf("Stay on the plateau!\n");
                 continue;
             }
@@ -175,7 +214,8 @@ do {
         case 'd':
             if (player->x < (SIZE_PLATEAU - 1)) {
                 player->x++;
-            } else {
+            } 
+            else {
                 printf("Stay on the plateau!\n");
                 continue;
             }
@@ -193,14 +233,14 @@ do {
     print_Plateau(copied_gameboard);
     printf("Card: %d\n", card);
 
-    // Update copied_gameboard box with corresponding gameboard value
-    copied_gameboard[player->y][player->x] = card;
-
     switch (card) {
         case ZOMBIE:
             if (player->weapon == TORCH) {
                 printf("Congratulations! You have killed a zombie!\n");
-            } else {
+              copied_gameboard[player->y][player->x] = gameboard[player->y][player->x];
+              print_Plateau(copied_gameboard);
+            } 
+            else {
                 printf("You have been killed by a zombie. Game over.\n");
                 player->alive = 0;
                 return;
@@ -210,7 +250,10 @@ do {
         case TROLL:
             if (player->weapon == AXE) {
                 printf("Congratulations! You have killed a troll!\n");
-            } else {
+              copied_gameboard[player->y][player->x] = gameboard[player->y][player->x];
+              print_Plateau(copied_gameboard);
+            } 
+            else {
                 printf("You have been killed by a troll. Game over.\n");
                 player->alive = 0;
                 return;
@@ -220,7 +263,10 @@ do {
         case BASILISK:
             if (player->weapon == SHIELD) {
                 printf("Congratulations! You have killed a basilisk!\n");
-            } else {
+                copied_gameboard[player->y][player->x] = gameboard[player->y][player->x];
+                print_Plateau(copied_gameboard);
+            } 
+            else {
                 printf("You have been killed by a basilisk. Game over.\n");
                 player->alive = 0;
                 return;
@@ -230,7 +276,10 @@ do {
         case HARPY:
             if (player->weapon == BOW) {
                 printf("Congratulations! You have killed a harpy!\n");
-            } else {
+              copied_gameboard[player->y][player->x] = gameboard[player->y][player->x];
+              print_Plateau(copied_gameboard);
+            } 
+            else {
                 printf("You have been killed by a harpy. Game over.\n");
                 player->alive = 0;
                 return;
@@ -240,33 +289,52 @@ do {
         case FIRE_SWORD:
             if (player->jrole == WARRIOR) {
                 printf("Congratulations! You have found your secret weapon!\n");
+              copied_gameboard[player->y][player->x] = gameboard[player->y][player->x];
+              print_Plateau(copied_gameboard);
                 player->special_obj++;
+            } 
+            else {
+              printf("You are on the warrior's weapon space");
             }
             break;
 
         case STICK:
             if (player->jrole == RANGER) {
                 printf("Congratulations! You have found your secret weapon!\n");
+                copied_gameboard[player->y][player->x] = gameboard[player->y][player->x];
+                print_Plateau(copied_gameboard);
                 player->special_obj++;
+            } else {
+              printf("You are on the warrior's weapon space");
             }
             break;
 
         case FORBIDDEN_GRIMOIRE:
             if (player->jrole == MAGE) {
                 printf("Congratulations! You have found your secret weapon!\n");
+                copied_gameboard[player->y][player->x] = gameboard[player->y][player->x];
+                print_Plateau(copied_gameboard);
                 player->special_obj++;
+            } else {
+              printf("You are on the mage's weapon space");
             }
             break;
 
         case SLEEP_DAGGER:
             if (player->jrole == THIEF) {
                 printf("Congratulations! You have found your secret weapon!\n");
+                copied_gameboard[player->y][player->x] = gameboard[player->y][player->x];
+                print_Plateau(copied_gameboard);
                 player->special_obj++;
+            } else {
+              printf("You are on the thief's weapon space");
             }
             break;
 
         case TREASURE_CHEST:
             printf("Congratulations! You have found a legendary chest!\n");
+            copied_gameboard[player->y][player->x] = gameboard[player->y][player->x];
+            print_Plateau(copied_gameboard);
             player->chest++;
             break;
 
@@ -276,17 +344,29 @@ do {
             scanf("%d", &player->x);
             printf("Enter y coordinate: ");
             scanf("%d", &player->y);
+          copied_gameboard[player->y][player->x] = gameboard[player->y][player->x];
+            print_Plateau(copied_gameboard);
             break;
 
         case TRANSMUTATION_TOTEM:
             printf("You have found the transmutation totem. Choose a hidden square:\n");
-            player->alive = 0; // Le joueur n'est pas mort, mais le tour se termine ici.
+            copied_gameboard[player->y][player->x] = gameboard[player->y][player->x];
+            print_Plateau(copied_gameboard);
+            player->alive = 0; // The player is not dead, but the game turn ends here
+            if (player->alive == 0 && player->jrole == RANGER) {
+            player->jrole = WARRIOR;
+            printf("Player %s lost!\n", role_names[RANGER]);
+            printf("Next player: %s\n", role_names[WARRIOR]);
+            }
+
             break;
 
         default:
             break;
     }
+  
 } while (player->alive && !player_win(*player));
+  copied_gameboard[player->y][player->x] = gameboard[player->y][player->x];
 }
 
 int player_win(Player player){
